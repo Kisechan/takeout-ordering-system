b69a399f73704056af4667e6b2163741ba30fe54
Kisechan <kisechan_@outlook.com>
2025-05-19 23:34:14 +0800

    完善修改

--- src/main/java/com/takeout/util/IconUtil.java
+++ src/main/java/com/takeout/util/IconUtil.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class IconUtil {
     public static void setWindowIcon(JFrame frame) {
-        ImageIcon icon = new ImageIcon(IconUtil.class.getResource("/logo.png"));
-        frame.setIconImage(icon.getImage());
+//        ImageIcon icon = new ImageIcon(IconUtil.class.getResource("/logo.png"));
+//        frame.setIconImage(icon.getImage());
     }
 }
